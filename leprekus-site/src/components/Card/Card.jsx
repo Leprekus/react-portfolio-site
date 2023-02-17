828ec2d2f4a2c2f67c0d45886eb9739e4a488e51
Leprekus <leprekus@yahoo.com>
2023-02-16 21:35:06 -0600

    create list components

--- leprekus-site/src/components/Card/Card.jsx
+++ leprekus-site/src/components/Card/Card.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Cardheader from './Cardheader';
 
 function Card({ children, img, header }) {
+  const divRef = useRef(null)
+  const handleExpand = () => {
+    divRef.current.scrollTo(0,0)
+    divRef.current.style.overflowY === 'scroll' ?
+    divRef.current.style.overflowY = 'hidden':
+    divRef.current.style.overflowY = 'scroll'
+
+    if(divRef.current.style.height === '75%') {
+      return divRef.current.style.height = '33%'
+    }
+    return divRef.current.style.height = '75%'
+    
+  }
+  
   return (
     //change width and height
     <div
@@ -11,11 +25,12 @@ function Card({ children, img, header }) {
       <div
         id='custom-scrollbar'
         className='w-full absolute left-0 bottom-0 rounded-b-md rounded-tl-3xl rounded-tr-sm
-        h-1/3 bg-zinc-100 hover:h-3/4 hover:ease-in ease-out duration-200
-        
-        overflow-hidden hover:overflow-y-scroll scroll-smooth
+        h-1/3 bg-zinc-100  hover:ease-in ease-out duration-200
+      
+        overflow-hidden cursor-pointer scroll-smooth
         flex justify-center text-neutral-500
         '
+        onClick={handleExpand} ref={divRef}
         >
         <Cardheader img={img} header={header}/>
         <div className='text-xs relative w-full pl-5 top-24'>
---
+++ leprekus-site/src/components/List/Listholder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Listholder() {
+  return (
+    <div>Listholder</div>
+  )
+}
+
+export default Listholder
