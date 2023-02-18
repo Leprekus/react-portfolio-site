74da527217718941035d72c952b70ebe0808550b
Leprekus <leprekus@yahoo.com>
2023-02-17 20:16:15 -0600

    add names to inputs

--- leprekus-site/src/pages/Contact.jsx
+++ leprekus-site/src/pages/Contact.jsx
@@ -9,11 +9,11 @@ function Contact() {
       <h1>Contact <span className='text-pink-400'>Me.</span></h1>
       <form className='flex flex-col items-start w-full
       md:w-1/2' netflify name='contact' method='POST'>
-        <input className='w-1/2 bg-[#20232f] focus:outline-none' type='name' placeholder="Name" />
+        <input className='w-1/2 bg-[#20232f] focus:outline-none' type='name' name='name' placeholder="Name" />
         <hr className='h-px my-4 bg-slate-600 border-0 w-3/4'/>
-        <input className='w-1/2 bg-[#20232f] focus:outline-none' type='email' placeholder="Email" />
+        <input className='w-1/2 bg-[#20232f] focus:outline-none' type='email' name='email' placeholder="Email" />
         <hr className='h-px my-4 bg-slate-600 border-0 w-3/4'/>
-        <input className='w-1/2 bg-[#20232f] focus:outline-none' type='text' placeholder="Message" />
+        <input className='w-1/2 bg-[#20232f] focus:outline-none' type='text' name='message' placeholder="Message" />
         <hr className='h-px my-4 bg-slate-600 border-0 w-3/4'/>
         <input className='w-1/2 focus:outline-none cursor-pointer
         px-4 py-2 bg-pink-400 rounded-full hover:bg-pink-600
